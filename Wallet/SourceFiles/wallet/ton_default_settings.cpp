db9613c7f46e6d53b3248d98ffe302a53678197c
Rexagon <reide740@gmail.com>
2021-01-28 03:34:53 +0300

    Add custom DePools. Migrate to TIP3 tokens

--- Wallet/SourceFiles/wallet/ton_default_settings.cpp
+++ Wallet/SourceFiles/wallet/ton_default_settings.cpp
@@ -13,26 +13,24 @@
 namespace Wallet {
 
 Ton::Settings GetDefaultSettings() {
-	auto result = Ton::Settings();
+  auto result = Ton::Settings();
 
-	auto main = QFile(":/config/mainnet.config.json");
-	main.open(QIODevice::ReadOnly);
-	result.main.config = main.readAll();
-	result.main.blockchainName = "mainnet";
-	result.main.configUrl = "https://freeton.broxus.com/mainnet.config.json";
-	result.main.tokenContractAddress = "0:08b697bafe589f5b14c6ce4f9cf4ff0a865a6f0c5a8371ea29ffe43643eb9fe4";
+  auto main = QFile(":/config/mainnet.config.json");
+  main.open(QIODevice::ReadOnly);
+  result.main.config = main.readAll();
+  result.main.blockchainName = "mainnet";
+  result.main.configUrl = "https://freeton.broxus.com/mainnet.config.json";
 
-	auto test = QFile(":/config/testnet.config.json");
-	test.open(QIODevice::ReadOnly);
-	result.test.config= test.readAll();
-	result.test.blockchainName = "net.ton.dev";
-	result.test.configUrl = "https://freeton.broxus.com/testnet.config.json";
-	result.test.tokenContractAddress = "0:08b697bafe589f5b14c6ce4f9cf4ff0a865a6f0c5a8371ea29ffe43643eb9fe4";
+  auto test = QFile(":/config/testnet.config.json");
+  test.open(QIODevice::ReadOnly);
+  result.test.config = test.readAll();
+  result.test.blockchainName = "net.ton.dev";
+  result.test.configUrl = "https://freeton.broxus.com/testnet.config.json";
 
-	result.useNetworkCallbacks = false;
-	result.useTestNetwork = false;
-	result.version = 0;
-	return result;
+  result.useNetworkCallbacks = false;
+  result.useTestNetwork = false;
+  result.version = 0;
+  return result;
 }
 
-} // namespace Wallet
+}  // namespace Wallet
